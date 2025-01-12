8a7ed0236457c47996d11879e8a2c377e6f861e3
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2025-01-12 17:00:45 +0700

    renew task

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -7,7 +7,7 @@ const Task = () => {
     <div className="flex bg-gradient-to-b from-[#004D40] via-[#00897B] to-[#4DB6AC]">
       <Sidebar />
       <main>
-        <TabelKaryawan/>
+        
       </main>
     </div>
   );
